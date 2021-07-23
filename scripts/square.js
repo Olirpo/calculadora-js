f7f99deeb922f4c849c90d998824ad4ed6b3bcfa
Olirpo <olvatgamer@gmail.com>
2021-07-23 01:48:25 -0300

    applied an poo programming in triangle formulas

--- scripts/square.js
+++ scripts/square.js
@@ -1,12 +1,37 @@
-const perimetroCuadrado = document.getElementById('pericuadrado');
-const areaCuadrado = document.getElementById('areacuadrado');
-document.getElementById('botonCuadrado').addEventListener("click",()=>{
-    var ladoCuadrado = parseFloat(document.getElementById('ladocuadrado').value)
-    if(ladoCuadrado > 0){
-        return perimetroCuadrado.value = (ladoCuadrado*4).toFixed(2),
-        areaCuadrado.value = (ladoCuadrado**2).toFixed(2);
+///////// DOM MANIPULATION CONSTS //////////////
+const respuestaCuadrado = document.getElementById('resultadoCuadrado');
+const resultadoTriangulo = document.getElementById('resultadoTriangulo');
+
+
+//////// SQUARE FUNCTIONS //////////
+const perimetroCuadrado = ()=>{
+    let lado = parseFloat(document.getElementById('ladocuadrado').value);
+    if (lado>0){
+        let perimetro = lado*4;
+    respuestaCuadrado.innerHTML = `El perimetro de tu cuadrado es de <strong style="color:black">${perimetro}cm</strong>`;
+    }
+    else{
+        alert('Introduzca un valor correcto')
     }
     
-    return perimetroCuadrado.value = "ERR INTRODUZCA LOS NUMEROS CORRECTAMENTE",
-        areaCuadrado.value = "ERR INTRODUZCA LOS NUMEROS CORRECTAMENTE"
-})
+}
+const areaCuadrado = ()=>{
+    let lado = parseFloat(document.getElementById('ladocuadrado').value);
+    let area = lado**2;
+    respuestaCuadrado.innerHTML = `El area de tu cuadrado es de <strong style="color:black">${area}cm²</strong>`
+}
+///////// TRIANGLE FUNCTIONS //////////
+const perimetroTriangulo = ()=>{
+    let base = parseFloat(document.getElementById('basetriangulo').value);
+    let ladoA = parseFloat(document.getElementById('ladoatriangulo').value);
+    let ladoB = parseFloat(document.getElementById('ladobtriangulo').value);
+    let triangulo = new Triangulo(base, ladoA, ladoB);
+    if (triangulo.isValid()){
+        let perimetro = triangulo.perimetro()
+        resultadoTriangulo.innerHTML = `El perimetro de tu triangulo es igual a 
+                                    <strong style="color:black">${perimetro}cm</strong>`;
+    }
+    else{
+        alert('Su triangulo no es valido, por favor introduzca los valores correctamente')
+    }
+}
--- scripts/triangle.js
+++ scripts/triangle.js
@@ -1,28 +1,18 @@
-const perimetroTriangulo = document.getElementById('perimetrotriangulo');
-const alturaTriangulo = document.getElementById('alturatriangulo');
-const areaTriangulo = document.getElementById('areatriangulo');
 
-document.getElementById('botonTriangulo').addEventListener('click',()=>{
-    const baseTriangulo = parseFloat(document.getElementById('basetriangulo').value);
-    const ladoATriangulo = parseFloat(document.getElementById('ladoatriangulo').value);
-    const ladoBTriangulo = parseFloat(document.getElementById('ladobtriangulo').value);
-    console.log(ladoBTriangulo)
-        // this functions validates if the triangle is corret, if any value its <=0 returns true
-    if (validateTriangle(baseTriangulo, ladoATriangulo, ladoBTriangulo)){
-        return perimetroTriangulo.value = "ERR INTRODUZCA LOS NUMEROS CORRECTAMENTE",
-        alturaTriangulo.value = "ERR INTRODUZCA LOS NUMEROS CORRECTAMENTE",
-        areaTriangulo.value = "ERR INTRODUZCA LOS NUMEROS CORRECTAMENTE";
+
+class Triangulo{
+    constructor (base, ladoA, ladoB){
+        this.base = base;
+        this.ladoA = ladoA;
+        this.ladoB = ladoB;
     }
-    perimetroTriangulo.value = baseTriangulo + ladoATriangulo + ladoBTriangulo;
-    alturaTriangulo.value = Math.sqrt((baseTriangulo/2)**2);
-    areaTriangulo.value = (baseTriangulo * alturaTriangulo.value) / 2;
-});
-
-function validateTriangle(base, A, B){
-    if (A + B > base && base > 0) return false;
-    return true;
-   /*  if (isNaN(A) || isNaN(B) || isNaN(base)) return true; 
-    if (A+B <= base) return true;
-    if(base<=0 || A<=0 || B <= 0) return true;
-    return false */
+    isValid(){
+        if(this.ladoA < this.ladoB + this.base && this.ladoB < this.ladoA + this.base && this.base < this.ladoA + this.ladoB){
+            return true
+        }
+        return false;
+    }
+    perimetro(){
+        return this.base + this.ladoA + this.ladoB
+    }
 }
